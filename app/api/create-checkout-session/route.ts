aa4c57e535db77722c0ec5a963df78542dce60ef
suksham11 <sukshamk170@example.com>
2025-01-09 18:33:52 +0530

    routes, pages , hooks

---
+++ app/api/create-checkout-session/route.ts
@@ -0,0 +1,37 @@
+import { NextResponse } from "next/server";
+import Stripe from 'stripe'
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
+    apiVersion: "2024-12-18.acacia"
+});
+
+export async function POST(req: Request){
+  try{
+    const {priceId, UserId} = await req.json();
+    if(!priceId || !UserId){
+        return NextResponse.json(
+            {error: "Missing priceId or userId"},
+            {status: 400}
+        );
+    }
+    const session = await stripe.checkout.sessions.create({
+        mode: 'subscription',
+        payment_method_types: ['card'],
+        line_items: [{
+            price: priceId,
+            quantity: 1,
+       }, ],
+       success_url: `${process.env.NEXT_PUBLIC_BASE_URL}/generate?session_id={CHECKOUT_SESSION_ID}`,
+       cancel_url: `${process.env.NEXT_PUBLIC_BASE_URL}/pricing`,
+       client_reference_id: UserId,
+    })
+    return NextResponse.json({sessionId: session.id
+    })
+  }catch(error: any){
+    console.error("Error creating checkout session:", error);
+    return NextResponse.json(
+      { error: "Error creating checkout session", details: error.message },
+      { status: 500 }
+    );
+
+  }
+}
